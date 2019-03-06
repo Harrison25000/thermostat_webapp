aeda22afac09daa71b756c3f00736bab0d4cec4e
Oliver <ofcharrison@gmail.com>
2019-03-06 17:35:00 +0000

    up to JQuery

---
+++ spec/thermostatSpec.js
@@ -0,0 +1,78 @@
+'use strict';
+
+describe('Feature Test:', function () {
+var thermostat;
+
+beforeEach(function() {
+  thermostat = new thermoStat();
+});
+
+it('thermostat starts at 20', function(){
+expect(thermostat.temp).toEqual(20);
+});
+
+it('increase adds one degree to thermostat temp', function(){
+thermostat.increase()
+console.log(thermostat.temp)
+expect(thermostat.temp).toEqual(21);
+});
+
+it('decreases the thermostat by one degree', function(){
+  thermostat.decrease()
+  console.log(thermostat.temp)
+  expect(thermostat.temp).toEqual(19);
+});
+
+it('cannot go lower than 10', function(){
+  thermostat.temp = 10
+  console.log(thermostat.temp)
+  console.log(thermostat.decrease())
+  expect(thermostat.decrease()).toEqual('Limbo bar at minimum!');
+});
+
+it('cannot go higher than 32 if powersave off', function(){
+  thermostat.powersaveoff()
+  thermostat.temp = 32
+  console.log(thermostat.temp)
+  console.log(thermostat.increase())
+  expect(thermostat.increase()).toEqual('Limbo bar at maximum!');
+});
+
+it('cannot go higher than 25 on powersave', function(){
+  thermostat.powersaveoff();
+  thermostat.powersaveon();
+  thermostat.temp = 25;
+  console.log(thermostat.temp);
+  console.log(thermostat.increase());
+  expect(thermostat.increase()).toEqual('Limbo bar at maximum');
+});
+
+it('can go higher than 25 on powersave off', function(){
+  console.log('-----powersaveoff-----')
+  thermostat.powersaveoff()
+  thermostat.temp = 25
+  console.log(thermostat.temp)
+  thermostat.increase()
+  expect(thermostat.temp).toEqual(26)
+});
+
+it('can reset back to 20 degress', function(){
+  console.log('---!---')
+  thermostat.increase()
+  console.log(thermostat.temp)
+  thermostat.reset()
+  console.log(thermostat.temp)
+  expect(thermostat.temp).toEqual(20)
+  console.log('---!---')
+});
+
+it('can see energy usage', function(){
+  console.log('---&---')
+  thermostat.temp
+  console.log(thermostat.energy())
+  expect(thermostat.energy()).toEqual('medium-usage')
+  console.log('---&---')
+});
+
+
+});
---
+++ src/thermostat.js
@@ -0,0 +1,40 @@
+var thermoStat = function (){
+  this.temp = 20
+  this.powersavemode = true
+};
+
+thermoStat.prototype.powersaveoff = function(){
+  this.powersavemode = false
+};
+thermoStat.prototype.powersaveon = function(){
+  this.powersavemode = true
+};
+
+thermoStat.prototype.increase = function() {
+  if(this.powersavemode == true && this.temp == 25)
+  {return 'Limbo bar at maximum'}
+  else if(this.temp == 32)
+  {return 'Limbo bar at maximum!'}
+  else
+  {this.temp ++};
+};
+
+thermoStat.prototype.decrease = function() {
+  if(this.temp == 10)
+  {return 'Limbo bar at minimum!'}
+  else
+  {this.temp --};
+};
+
+thermoStat.prototype.reset = function() {
+  this.temp = 20
+};
+
+thermoStat.prototype.energy = function() {
+  if(this.temp<18)
+  {return 'low-usage'}
+  else if(this.temp>18 && this.temp<25)
+  {return 'medium-usage'}
+  else
+  {return 'high-usage'};
+};
